725777b55ca97ff6a56992217984a083054a147d
Paulo Victor da Silva <paulovmel@gmail.com>
2020-10-27 11:13:12 -0300

    reboot project

--- src/pages/Profile/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { Container } from './styles';
-
-const Profile: React.FC = () => {
-  return <Container />;
-}
-
-export default Profile;
--- src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import AppRoutes from './app.routes';
-
-const Routes: React.FC = () => {
-  return <AppRoutes />;
-};
-
-export default Routes;
