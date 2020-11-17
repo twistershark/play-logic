9c066e1f7bcd0de7294ddb1980dc68a93921786b
Paulo Victor da Silva <paulovmel@gmail.com>
2020-11-17 12:12:19 -0300

    style: adiciona responsividade

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,52 +1,99 @@
 import React from 'react';
 
 import {
-Container, 
-BackNavigation,
-BackArrow,
-ProfileView,
-ProfileImage,
-UserImage,
-UserName,
-Options,
-TextOptions,
-Score
-} from './styles';
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 
-import backArrow from '../../assets/backarrow.png'
-import userImage from '../../assets/userImage.png'
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import ScoreList from '../../components/ScoreList'
+import { useNavigation } from '@react-navigation/native';
+import backArrow from '../../assets/backarrow.png';
+import userImage from '../../assets/userImage.png';
 
-const Profile = ({navigation}) => {
+import { useAuth } from '../../hooks/auth';
+
+import ScoreList from '../../components/ScoreList';
+
+const Profile = () => {
   const score = [
-    {key: '1', star:3},
-    {key: '2', star:2},
-    {key: '3', star:-1}, 
-  ]
-  const user = true; 
-  return(
-      <Container>
-          <BackNavigation onPress = {() => navigation.goBack()} >
-            <BackArrow source = {backArrow}/>
-          </BackNavigation>
-          <ProfileView> 
-            <ProfileImage>
-                <UserImage source = {userImage}/>
-            </ProfileImage>
-            <UserName>Nome do Usuário</UserName>
-            <Options>
-                <TextOptions>{ user? "Logout" : "Login" }</TextOptions>
-            </Options>
-          </ProfileView>
-          <Score>
-            <ScoreList score= {score} /> 
-          </Score>
-      </Container>
-  );
+    { key: '1', star: 3 },
+    { key: '2', star: 2 },
+    { key: '3', star: -1 },
+  ];
 
+  const { user } = useAuth();
+
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.backNavigation} onPress={() => navigation.goBack()}>
+        <Image source={backArrow} />
+      </TouchableOpacity>
+      <View style={styles.profileView}>
+        <View style={styles.profileImage}>
+          <Image style={styles.userImage} source={userImage} />
+        </View>
+        <Text style={styles.userName}>Nome do Usuário</Text>
+        <View>
+          <Text style={styles.textOptions}>{ user ? 'Logout' : 'Login' }</Text>
+        </View>
+      </View>
+      <View style={styles.score}>
+        <ScoreList score={score} />
+      </View>
+    </SafeAreaView>
+  );
 };
 
-
-
 export default Profile;
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  backNavigation: {
+    marginTop: 32,
+    marginLeft: 12,
+  },
+
+  profileView: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+
+  userImage: {
+    width: 170,
+    height: 170,
+  },
+
+  userName: {
+    fontSize: 24,
+    color: 'white',
+  },
+
+  textOptions: {
+    color: 'white',
+  },
+
+  score: {
+    marginTop: 66,
+    alignItems: 'center',
+  },
+});
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import {
-  Container,
-  Logo,
-  FormContainer,
-  SocialLoginContainer,
-  SocialLogin,
-  SocialLogo,
-  SocialLoginText,
-  SignUpButton,
-  SignUpText,
-} from './styles';
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import { useNavigation } from '@react-navigation/native';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -40,33 +41,89 @@ const SignIn = () => {
         keyboardShouldPersistTaps="handled"
         contentContainerStyle={{ flex: 1 }}
       >
-        <Container>
-          <Logo source={logoImg} />
-          <FormContainer>
+        <SafeAreaView style={styles.container}>
+          <Image style={styles.logo} source={logoImg} />
+          <View style={styles.formContainer}>
             <Input value={email} placeholder="Email" onChangeText={(userEmail) => setEmail(userEmail)} />
             <Input value={password} placeholder="Senha" onChangeText={(userPassword) => setPassword(userPassword)} secureTextEntry />
-          </FormContainer>
+          </View>
           <Button onPress={() => login(email, password)}>Entrar</Button>
 
-          <SocialLoginContainer>
-            <SocialLogin onPress={() => googleLogin()}>
-              <SocialLogo source={googleLogo} />
-              <SocialLoginText>Entrar com a conta Google</SocialLoginText>
-            </SocialLogin>
+          <View style={styles.socialLoginContainer}>
+            <TouchableOpacity style={styles.socialLogin} onPress={() => googleLogin()}>
+              <Image style={styles.socialLogo} source={googleLogo} />
+              <Text style={styles.socialLoginText}>Entrar com a conta Google</Text>
+            </TouchableOpacity>
 
-            <SocialLogin onPress={() => facebookLogin()}>
-              <SocialLogo source={facebookLogo} />
-              <SocialLoginText>Entrar com Facebook</SocialLoginText>
-            </SocialLogin>
-          </SocialLoginContainer>
+            <TouchableOpacity style={styles.socialLogin} onPress={() => facebookLogin()}>
+              <Image style={styles.socialLogo} source={facebookLogo} />
+              <Text style={styles.socialLoginText}>Entrar com Facebook</Text>
+            </TouchableOpacity>
+          </View>
 
-          <SignUpButton onPress={() => navigation.navigate('SignUp')}>
-            <SignUpText>Não tem cadastro? Cadastre-se aqui!</SignUpText>
-          </SignUpButton>
-        </Container>
+          <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('SignUp')}>
+            <Text style={styles.signUpText}>Não tem cadastro? Cadastre-se aqui!</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
       </ScrollView>
     </KeyboardAvoidingView>
   );
 };
 
 export default SignIn;
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 0,
+    paddingBottom: 0,
+    paddingLeft: '1.5rem',
+    paddingRight: '1.5rem',
+  },
+
+  logo: {
+    marginTop: '7rem',
+  },
+
+  formContainer: {
+    marginTop: '2.5rem',
+  },
+
+  loginButton: {},
+
+  socialLoginContainer: {
+    marginTop: '2rem',
+    width: '100%',
+  },
+
+  socialLogin: {
+    backgroundColor: '#426B00',
+    borderRadius: '0.625rem',
+    width: '100%',
+    height: '3.125rem',
+    marginTop: '1rem',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  socialLogo: {
+    marginRight: '3.125rem',
+    marginLeft: '1.25rem',
+  },
+
+  socialLoginText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+
+  signUpButton: {
+    marginTop: '2.5rem',
+  },
+
+  signUpText: {
+    fontSize: '1rem',
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
