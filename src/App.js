1982173c9d168bec067d34459d7d1fe2de17c84b
Paulo Victor da Silva <paulovmel@gmail.com>
2020-11-17 12:17:23 -0300

    style: adiciona responsividade

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Routes from './routes';
 import { AuthProvider } from './hooks/auth';
 
 const App = () => (
+
   <NavigationContainer>
     <StatusBar barStyle="dark-content" backgroundColor="#538900" translucent />
     <AuthProvider>
