9d031a30ce47be14975f9a2063e64fe1dcdc9f74
Kleidson <kleidsonalves15@gmail.com>
2020-11-24 18:33:00 -0300

    Adição de mapa em Stage

--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,18 +1,38 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import EStyleSheet from 'react-native-extended-stylesheet'; 
+
+import map from '../../assets/mapas/Mapa_Ver3_semBorda.png';
 
 const Stage = () => {
   return (
-    <SafeAreaView> 
+    <SafeAreaView style = {styles.container}> 
       <View>
+          <ImageBackground source = {map} style = {styles.imgMap}>
+          </ImageBackground>
+          <View>
 
+          </View>
       </View>
       <View>
-        
+          <Text>Área da main</Text>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
+
+const styles = EStyleSheet.create({
+  container: { 
+    flexDirection: 'row', 
+  },
+  imgMap: {
+    width: '29.3rem',
+    height: '18.7rem',
+    marginLeft: '0.8rem'
+  }
+
+})
 
 export default Stage;
--- src/pages/Stage1/index.js
+++ src/pages/Stage1/index.js
@@ -3,11 +3,15 @@ import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import Stage from '../../components/Stage'
+import { View } from 'react-native';
 
 const Stage1 = () => {
   const navigation = useNavigation();
   return (
-   <Stage/> 
+    <View>
+      <Stage/>   
+    </View>
+    
   );
 };
 
