a88394ef023da82a7486649f51c6a77652741193
Kleidson <kleidsonalves15@gmail.com>
2020-11-24 18:21:17 -0300

    Criação: Componente Stage

---
+++ src/components/Stage/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Stage = () => {
+  return (
+    <SafeAreaView> 
+      <View>
+
+      </View>
+      <View>
+        
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Stage;
--- src/pages/Stage1/index.js
+++ src/pages/Stage1/index.js
@@ -1,39 +1,14 @@
 import React from 'react';
-import {
-  View, Text, TouchableOpacity, Image,
-} from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
-import EStyleSheet from 'react-native-extended-stylesheet';
-import backArrow from '../../assets/backarrow.png';
+import Stage from '../../components/Stage'
 
 const Stage1 = () => {
   const navigation = useNavigation();
   return (
-    <View>
-      <View>
-        <TouchableOpacity style={styles.backNavigation} onPress={() => navigation.goBack()}>
-          <Image source={backArrow} />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.container}>
-        <Text style={{ color: 'red' }}>Bem vindo à Fase 1</Text>
-      </View>
-    </View>
+   <Stage/> 
   );
 };
-
-const styles = EStyleSheet.create({
-  backNavigation: {
-    marginTop: '2rem',
-    marginLeft: '0.75rem',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Stage1;
