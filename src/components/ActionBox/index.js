93e41f09b35ac7843c541ebb0c505027f945ea53
Kleidson <kleidsonalves15@gmail.com>
2020-11-24 18:51:28 -0300

    Criação: Componente ActionBox

---
+++ src/components/ActionBox/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+
+import action from '../../assets/botoes/bt-action.png'; 
+import eat from '../../assets/botoes/bt-eat-2.png';
+import jump from '../../assets/botoes/bt-jump-1.png'; 
+
+
+const ActionBox = () => {
+  const [image, setImage] = useState(action); 
+
+  const change = () => {
+    if(image === action || image ===jump)
+      setImage(eat);
+    else 
+      setImage(jump); 
+  }
+  
+  return (
+    <View>
+      <TouchableOpacity onPress={change}>
+        <Image />
+      </TouchableOpacity> 
+    </View>
+  )
+}
+
+export default ActionBox; 
