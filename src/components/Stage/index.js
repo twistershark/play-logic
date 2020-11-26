fbc9376062311062da29b7877476311c878ba722
Lucas Gabriel <lucasbsb100@gmail.com>
2020-11-26 18:32:16 -0300

    Em progresso, já é possivel arrastar e soltar o componente actionbox

--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import {
-  View, Text, ImageBackground, TouchableOpacity, Image, FlatList,
+  View,
+  Text, 
+  ImageBackground, 
+  TouchableOpacity, 
+  Image, 
+  Animated,
+  PanResponder,
+  Alert
 } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -13,19 +20,64 @@ import IfBox from './IfBox';
 import LoopBox from './LoopBox';
 import Main from './Main';
 
-import map from '../../assets/mapas/Mapa_Ver3_semBorda.png';
+import map from '../../assets/mapas/mapa_completo.png';
 import play from '../../assets/play.png';
 import reset from '../../assets/reset.png';
 import backArrow from '../../assets/backarrow.png';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 const Stage = () => {
   const navigation = useNavigation();
+  const pan = useRef(new Animated.ValueXY()).current;
+  const [instrunction, setInstrunction] = useState([]);
+  var array = [];
+
+  const onArea = (x,y) => {
+    if(x > 460 && y > 30 && y < 180){
+      array.push(1);
+      console.log(array);
+    }
+    Animated.spring(pan, {
+      toValue: { x: 0, y: 0 },
+      friction: 5,
+      useNativeDriver: false
+    }).start();
+
+  }
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value
+        });
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          { dx: pan.x, dy: pan.y },
+        ],{useNativeDriver: false},
+      ),
+      onPanResponderRelease: (e, gesture) => {
+        onArea(gesture.moveX, gesture.moveY)
+      }
+    })
+  ).current;
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <ImageBackground source={map} style={styles.imgMap} />
         <View style={styles.instructionSection}>
+          <Animated.View
+          style={{
+            transform: [{ translateX: pan.x }, { translateY: pan.y }]
+          }}
+          {...panResponder.panHandlers}
+          >
           <ActionBox />
+          </Animated.View>
           <MoveBox />
           <IfBox />
           <LoopBox />
