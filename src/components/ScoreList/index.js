0105b05054f849d2de5ebc6465076735afcdf11c
kleidson-alves <kleidsonalves15@gmail.com>
2020-11-29 13:53:11 -0300

    Incremento do array na main com handleAddToMain

--- src/components/ScoreList/index.js
+++ src/components/ScoreList/index.js
@@ -1,41 +1,45 @@
-import React from 'react'
+import React from 'react';
 
+import {
+  Container, Header, ItemHeader, ItemList, Text, ItemView, ImageScore, List,
+} from './styles';
 
-import {Container, Header, ItemHeader, ItemList, Text, ItemView, ImageScore, List} from './styles'
+import emptyStar from '../../assets/emptyStar.png';
+import star from '../../assets/star.png';
+import noStar from '../../assets/null.png';
 
-import emptyStar from '../../assets/emptyStar.png'
-import star from '../../assets/star.png'
-import noStar from '../../assets/null.png'
-
-
-
-const scoreList = ({score}) => {
-  const renderItem = (item) => {
-    return (
-      <ItemList style = {[item.key%2? {backgroundColor: '#592C15'}: {backgroundColor: '#7F4726'}]} >
-          <ItemView>
-            <Text>{item.key}</Text>
-          </ItemView>
-        <ItemView>
-          <ImageScore source = {item.star>-1? (item.star>0? star : emptyStar): noStar} style = {[item.star<0? {width: 35,
-  height: 5, marginLeft: 35 } : ' ' ]} />
-          <ImageScore source = {item.star>-1? (item.star>1? star : emptyStar): null} />
-          <ImageScore source = {item.star>-1? (item.star>2? star : emptyStar): null}/>
-        </ItemView>
-      </ItemList>
-    )
-  }
+const scoreList = ({ score }) => {
+  const renderItem = (item) => (
+    <ItemList style={[item.key % 2 ? { backgroundColor: '#592C15' } : { backgroundColor: '#7F4726' }]}>
+      <ItemView>
+        <Text>{item.key}</Text>
+      </ItemView>
+      <ItemView>
+        <ImageScore
+          source={item.star > -1 ? (item.star > 0 ? star : emptyStar) : noStar}
+          style={[item.star < 0 ? {
+            width: 35,
+            height: 5,
+            marginLeft: 35,
+          } : ' ']}
+        />
+        <ImageScore source={item.star > -1 ? (item.star > 1 ? star : emptyStar) : null} />
+        <ImageScore source={item.star > -1 ? (item.star > 2 ? star : emptyStar) : null} />
+      </ItemView>
+    </ItemList>
+  );
   return (
-    <Container> 
-      <Header > 
+    <Container>
+      <Header>
         <ItemHeader><Text>FASE</Text></ItemHeader>
-        <ItemHeader><Text>PONTOS</Text></ItemHeader> 
+        <ItemHeader><Text>PONTOS</Text></ItemHeader>
       </Header>
-      <List data = {score}
-      renderItem = {({item}) => renderItem(item)} /> 
+      <List
+        data={score}
+        renderItem={({ item }) => renderItem(item)}
+      />
     </Container>
-  )
-}
+  );
+};
 
-export default scoreList; 
-
+export default scoreList;
