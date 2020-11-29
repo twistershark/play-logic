d9565b0de373dd54f4b5fa74a6074e294eac90bc
kleidson-alves <kleidsonalves15@gmail.com>
2020-11-29 10:05:20 -0300

    Atualizaçao do PanResponder nos componentes de Stage

--- src/components/Stage/ActionBox/index.js
+++ src/components/Stage/ActionBox/index.js
@@ -1,5 +1,11 @@
-import React, { useState } from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  PanResponder,
+  Animated,
+} from 'react-native';
 
 import action from '../../../assets/botoes/bt-action.png';
 import eat from '../../../assets/botoes/bt-eat-2.png';
@@ -7,17 +13,63 @@ import jump from '../../../assets/botoes/bt-jump-1.png';
 
 const ActionBox = () => {
   const [image, setImage] = useState(action);
+  const pan = useRef(new Animated.ValueXY()).current;
 
   const change = () => {
-    if (image === action || image === jump) { setImage(eat); } else { setImage(jump); }
+    if (image === action) {
+      setImage(eat);
+    } else if (image === eat) {
+      setImage(jump);
+    } else {
+      setImage(action);
+    }
   };
 
+  const onArea = (x, y) => {
+    if (x > 460 && y > 30 && y < 180) {
+      // atualizar o valor do array global;
+    }
+    Animated.spring(pan, {
+      toValue: { x: 0, y: 0 },
+      friction: 5,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          x: pan.x._value,
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          { dx: pan.x, dy: pan.y },
+        ], { useNativeDriver: false },
+      ),
+      onPanResponderRelease: (e, gesture) => {
+        onArea(gesture.moveX, gesture.moveY);
+      },
+    }),
+  ).current;
+
   return (
-    <View>
-      <TouchableOpacity onPress={change}>
-        <Image source={image} />
-      </TouchableOpacity>
-    </View>
+    <Animated.View
+      style={{
+        transform: [{ translateX: pan.x }, { translateY: pan.y }],
+      }}
+      {...panResponder.panHandlers}
+    >
+      <View>
+        <TouchableOpacity onPress={change}>
+          <Image source={image} />
+        </TouchableOpacity>
+      </View>
+    </Animated.View>
   );
 };
 
--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -1,11 +1,9 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import {
   View,
   ImageBackground,
   TouchableOpacity,
   Image,
-  Animated,
-  PanResponder,
 } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -25,55 +23,15 @@ import backArrow from '../../assets/backarrow.png';
 
 const Stage = () => {
   const navigation = useNavigation();
-  const pan = useRef(new Animated.ValueXY()).current;
   const [instruction, setInstruction] = useState([]);
   const array = [];
-
-  const onArea = (x, y) => {
-    if (x > 460 && y > 30 && y < 180) {
-      array.push(1);
-      console.log(array);
-    }
-    Animated.spring(pan, {
-      toValue: { x: 0, y: 0 },
-      friction: 5,
-      useNativeDriver: false,
-    }).start();
-  };
-  const panResponder = useRef(
-    PanResponder.create({
-      onMoveShouldSetPanResponder: () => true,
-      onPanResponderGrant: () => {
-        pan.setOffset({
-          x: pan.x._value,
-          y: pan.y._value,
-        });
-      },
-      onPanResponderMove: Animated.event(
-        [
-          null,
-          { dx: pan.x, dy: pan.y },
-        ], { useNativeDriver: false },
-      ),
-      onPanResponderRelease: (e, gesture) => {
-        onArea(gesture.moveX, gesture.moveY);
-      },
-    }),
-  ).current;
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <ImageBackground source={map} style={styles.imgMap} />
         <View style={styles.instructionSection}>
-          <Animated.View
-            style={{
-              transform: [{ translateX: pan.x }, { translateY: pan.y }],
-            }}
-            {...panResponder.panHandlers}
-          >
-            <ActionBox />
-          </Animated.View>
+          <ActionBox />
           <MoveBox />
           <IfBox />
           <LoopBox />
