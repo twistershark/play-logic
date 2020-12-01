3e57faca825f83b9db428946f46105b5d6d5bcff
Lucas Gabriel <lucasbsb100@gmail.com>
2020-12-01 17:48:09 -0300

    adcionando starA

--- src/pages/Score/index.js
+++ src/pages/Score/index.js
@@ -4,21 +4,23 @@ import {
   View,
   Image,
   Text,
+  TouchableOpacity,
 } from 'react-native';
 
 import Modal from 'react-native-modal';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import Orientation from 'react-native-orientation-locker';
 import { useNavigation } from '@react-navigation/native';
 
 import menu from '../../assets/menu.png';
 import nextFase from '../../assets/nextFase.png';
 import reset from '../../assets/reset.png';
-import score from '../../assets/Score.png';
+import scoreText from '../../assets/Score.png';
+import star from '../../assets/star.png';
+import noStar from '../../assets/null.png';
 
-const ScorePage = () => {
-  const [visible, setVisible] = useState(false);
+const ScorePage = ({ isVisible, score }) => {
+  const [visible, setVisible] = useState(isVisible);
   Orientation.lockToLandscape();
   const navigation = useNavigation();
 
@@ -27,7 +29,7 @@ const ScorePage = () => {
       backgroundColor: 'white', flex: 1, justifyContent: 'center', alignItems: 'center',
     }}
     >
-      <TouchableOpacity onPress={() => { setVisible(true); }}>
+      <TouchableOpacity onPress={() => setVisible(true)}>
         <Text>
           Clicar
         </Text>
@@ -36,15 +38,20 @@ const ScorePage = () => {
         isVisible={visible}
       >
         <View style={styles.modal}>
-          <View style={{ padding: 20, alignItems: 'center' }}>
+          <View style={{ padding: 15, alignItems: 'center' }}>
             <View style={styles.container}>
-              <Image source={score} />
+              <Image source={scoreText} />
+            </View>
+            <View style={{ flexDirection: 'row', paddingBottom: 10 }}>
+              <Image source={score >= 1 ? star : noStar} style={styles.starView} />
+              <Image source={score >= 2 ? star : noStar} style={styles.starView} />
+              <Image source={score === 3 ? star : noStar} style={styles.starView} />
             </View>
             <View style={styles.rowButtons}>
-              <TouchableOpacity onPress={() => { setVisible(false); }} style={styles.button}>
+              <TouchableOpacity onPress={() => setVisible(false)} style={styles.button}>
                 <Image source={reset} />
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate('Stages')} style={styles.button}>
+              <TouchableOpacity onPress={() => { navigation.navigate('Stages'); setVisible(false); }} style={styles.button}>
                 <Image source={menu} />
               </TouchableOpacity>
               <TouchableOpacity onPress={() => {}} style={styles.button}>
@@ -82,9 +89,10 @@ const styles = EStyleSheet.create({
     flexDirection: 'row',
   },
   button: {
-    marginLeft: 40,
-    marginRight: 40,
-
+    marginHorizontal: 40,
+  },
+  starView: {
+    marginHorizontal: 10,
   },
 
 });
