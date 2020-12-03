12f77d6caeded46fbce2e8a72885ac7ece9d77e8
Lucas Gabriel <lucasbsb100@gmail.com>
2020-12-03 20:01:37 -0300

    correção da musica e sensibilidade

--- src/components/Stage/LoopBox/index.js
+++ src/components/Stage/LoopBox/index.js
@@ -52,7 +52,7 @@ const LoopBox = () => {
 
   const panResponder = useRef(
     PanResponder.create({
-      onMoveShouldSetPanResponder: (gesture) => !(gesture.dx > 2 && gesture.dy > 2),
+      onMoveShouldSetPanResponder: (gesture) => !(gesture.dx > 5 && gesture.dy > 5),
       onPanResponderGrant: () => {
         pan.setOffset({
           x: pan.x._value,
--- src/components/Stage/MoveBox/index.js
+++ src/components/Stage/MoveBox/index.js
@@ -53,7 +53,7 @@ const MoveBox = () => {
 
   const panResponder = useRef(
     PanResponder.create({
-      onMoveShouldSetPanResponder: (gesture) => !(gesture.dx > 2 && gesture.dy > 2),
+      onMoveShouldSetPanResponder: (gesture) => !(gesture.dx > 5 && gesture.dy > 5),
       onPanResponderGrant: () => {
         pan.setOffset({
           x: pan.x._value,
