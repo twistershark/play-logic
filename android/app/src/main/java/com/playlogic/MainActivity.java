73f9db6bfb3ee4e1299aca6e6e15313f94266820
Paulo Victor da Silva <paulovmel@gmail.com>
2020-12-03 16:14:57 -0300

    esconde navegacao no android

--- android/app/src/main/java/com/playlogic/MainActivity.java
+++ android/app/src/main/java/com/playlogic/MainActivity.java
@@ -6,14 +6,34 @@ import android.content.res.Configuration;
 import org.devio.rn.splashscreen.SplashScreen; 
 import android.os.Bundle;
 
+import android.os.Bundle;
+import android.view.View;
+
 public class MainActivity extends ReactActivity {
 
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
       SplashScreen.show(this);
+      hideNavigationBar();
       super.onCreate(savedInstanceState);
   }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+      super.onWindowFocusChanged(hasFocus);
+      if (hasFocus) {
+          hideNavigationBar();
+      }
+  }
+  
+  private void hideNavigationBar() {
+      getWindow().getDecorView().setSystemUiVisibility(
+          View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+          | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+  }
+
 
   @Override
   public void onConfigurationChanged(Configuration newConfig) {
